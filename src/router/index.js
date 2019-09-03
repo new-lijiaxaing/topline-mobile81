d183d08ca1306fe9660c611af660a0294d2b294d
牛亮 <nll@itcast.cn>
2019-09-03 17:11:40 +0800

    配置home的子路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Tabbar from '@/views/Tabbar.vue'
+import Home from '@/views/home/index.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      name: 'tabbar',
+      path: '/',
+      component: Tabbar,
+      children: [
+        {
+          name: 'home',
+          path: '',
+          component: Home
+        }
+      ]
+    },
     {
       path: '/login',
       name: 'login',
