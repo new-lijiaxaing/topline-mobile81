f08b4a24087b22596222ccb22058f01537800d6c
牛亮 <nll@itcast.cn>
2019-09-03 10:48:58 +0800

    登录功能-界面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    Login
+    <!-- 导航头 -->
+    <van-nav-bar
+      title="登录"
+    />
+    <!-- 输入框 -->
+    <van-cell-group>
+      <van-field v-model="value" left-icon="phone-o" placeholder="请输入手机号码" />
+      <van-field v-model="value" left-icon="star-o" placeholder="请输入验证码">
+        <van-button slot="button" type="default" size="small">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+    <div class="login-btn">
+      <van-button class="btn" type="info">登录</van-button>
+    </div>
   </div>
 </template>
 
@@ -10,6 +24,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login-btn {
+  padding: 20px;
+  .btn {
+    width: 100%;
+  }
+}
 </style>
