67559619d1925951c06c39264e214445eb74c849
牛亮 <nll@itcast.cn>
2019-09-03 11:23:05 +0800

    登录功能-登录成功-失败的提示

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -34,10 +34,14 @@ export default {
     // 点击按钮，处理登录
     async handleLogin () {
       try {
-        const res = await login(this.user)
-        console.log(res)
+        // data 就是接口返回数据中的data（因为响应拦截器做了处理）
+        const data = await login(this.user)
+        // 存储登录的状态
+        // 跳转到首页
+        this.$router.push('/')
+        this.$toast.success('登录成功')
       } catch (err) {
-        console.log(err)
+        this.$toast.fail('登录失败')
       }
     }
   }
