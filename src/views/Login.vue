6b353f79a0cbc2e01b0783fcdccc6e3662a6aad8
牛亮 <nll@itcast.cn>
2019-09-03 11:47:25 +0800

    登录功能-保持登录状态结束

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -20,6 +20,7 @@
 
 <script>
 import { login } from '@/api/user'
+import { mapMutations } from 'vuex'
 export default {
   name: 'Login',
   data () {
@@ -31,6 +32,7 @@ export default {
     }
   },
   methods: {
+    ...mapMutations(['setUser']),
     // 点击按钮，处理登录
     async handleLogin () {
       try {
@@ -39,9 +41,11 @@ export default {
         const data = await login(this.user)
         // 存储登录的状态
         // 1. vuex
-        this.$store.commit('setUser', data)
         // 2. 本地存储
-        
+        // --- 以上两件事儿 都是在store中完成----
+        // this.$store.commit('setUser', data)
+        this.setUser(data)
+
         // 跳转到首页
         this.$router.push('/')
         this.$toast.success('登录成功')
