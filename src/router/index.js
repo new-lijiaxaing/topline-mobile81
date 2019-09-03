5ef332849a171aef2b48a7f854a60438e26cbfdd
牛亮 <nll@itcast.cn>
2019-09-03 17:19:05 +0800

    tabbar组件

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      name: 'tabbar',
+      // name: 'tabbar',
       path: '/',
       component: Tabbar,
       children: [
--- src/views/Tabbar.vue
+++ src/views/Tabbar.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <router-view></router-view>
-    这是tab
+    
+    <van-tabbar route>
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/qa" icon="search">问答</van-tabbar-item>
+      <van-tabbar-item to="/video" icon="friends-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="setting-o">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Tabbar'
 }
 </script>
 
