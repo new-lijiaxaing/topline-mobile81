323b32fc36a804096810acc8ff68e1b428407e39
牛亮 <nll@itcast.cn>
2019-09-05 17:40:38 +0800

    不感兴趣

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -13,9 +13,9 @@
     closeOnClickOverlay
   >
     <van-cell-group v-show="!showReports">
-      <van-cell title="不感兴趣" icon="location-o"/>
+      <van-cell title="不感兴趣" icon="location-o" @click="handle('dislike')"/>
       <van-cell title="反馈垃圾内容" icon="location-o" is-link @click="showReports=true"/>
-      <van-cell title="拉黑作者" icon="location-o"/>
+      <van-cell title="拉黑作者" icon="location-o" @click="handle('blacklist')"/>
     </van-cell-group>
     <!-- 举报文章 -->
     <van-cell-group v-show="showReports">
@@ -28,6 +28,7 @@
 </template>
 
 <script>
+import { dislikeArticle } from '@/api/article'
 export default {
   name: 'MoreAction',
   props: {
@@ -46,9 +47,28 @@ export default {
       showReports: false
     }
   },
-  created () {
-    // 测试
-    console.log(this.article.art_id)
+  methods: {
+    // 点击所有cell的时候，都执行该方法
+    // 通过type判断具体要执行的操作
+    handle (type) {
+      switch (type) {
+        case 'dislike':
+          // 不感兴趣
+          this.dislike()
+          break
+        case 'blacklist':
+          break
+      }
+    },
+    // 不感兴趣
+    async dislike () {
+      try {
+        await dislikeArticle(this.article.art_id)
+        this.$toast.success('操作成功')
+      } catch (err) {
+        this.$toast.fail('操作失败')
+      }
+    }
   }
 }
 </script>
