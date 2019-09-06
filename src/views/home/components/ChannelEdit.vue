23bf06b1d4220c9094a1ec41cf6cc064b09e79a9
牛亮 <nll@itcast.cn>
2019-09-06 11:47:26 +0800

    频道管理-展示推荐频道

--- src/views/home/components/ChannelEdit.vue
+++ src/views/home/components/ChannelEdit.vue
@@ -43,15 +43,17 @@
     <van-cell title="推荐频道" label="点击添加频道" />
     <van-grid>
       <van-grid-item
-        v-for="value in 8"
-        :key="value"
-        text="文字"
+        v-for="channel in recommendChannels"
+        :key="channel.id"
+        :text="channel.name"
       />
     </van-grid>
   </van-popup>
 </template>
 
 <script>
+import { getAllChannels } from '@/api/channel'
+
 export default {
   name: 'ChannelEdit',
   props: {
@@ -59,15 +61,49 @@ export default {
       type: Boolean,
       required: true
     },
+    // 接收父组件传过来的我的频道
     channels: {
       type: Array,
       required: true
     }
   },
+  computed: {
+    // 推荐频道
+    recommendChannels () {
+      // 1. 获取我的频道中所有id组成的数组
+      // map() 遍历数组，返回一个新的数组，新数组中的元素由回调函数中返回的元素组成
+      const ids = this.channels.map((channel) => {
+        return channel.id
+      })
+      // 2. 过滤所有频道，把频道id出现在上面数组中的项去除
+      // filter() 把满足条件的item，返回组成一个新的数组
+      return this.allChannels.filter((channel) => {
+        // includes()   es6新增，判断数组中是否包含某一项
+        return !ids.includes(channel.id)
+      })
+    }
+  },
   data () {
     return {
       // 是否是编辑模式
-      isEdit: false
+      isEdit: false,
+      // 存储所有频道
+      allChannels: []
+    }
+  },
+  created () {
+    // 加载所有的频道列表
+    this.loadAllChannels()
+  },
+  methods: {
+    // 加载所有的频道列表
+    async loadAllChannels () {
+      try {
+        const data = await getAllChannels()
+        this.allChannels = data.channels
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 }
