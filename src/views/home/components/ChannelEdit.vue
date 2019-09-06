694fd1edc2ad626206cd4f42b32ce4d046cc526f
牛亮 <nll@itcast.cn>
2019-09-06 11:23:33 +0800

    频道管理-控制编辑模式

--- src/views/home/components/ChannelEdit.vue
+++ src/views/home/components/ChannelEdit.vue
@@ -12,7 +12,17 @@
         round
         type="danger"
         size="mini"
+        v-show="!isEdit"
+        @click="isEdit=true"
       >编辑</van-button>
+
+      <van-button
+        round
+        type="danger"
+        size="mini"
+        v-show="isEdit"
+        @click="isEdit=false"
+      >完成</van-button>
     </van-cell>
     <van-grid>
       <van-grid-item
@@ -20,10 +30,12 @@
         :key="value"
         text="文字"
       >
+      <!-- 关闭按钮 -->
         <van-icon
           slot="icon"
           class="close-icon"
           name="close"
+          v-show="isEdit"
         />
       </van-grid-item>
     </van-grid>
@@ -50,6 +62,8 @@ export default {
   },
   data () {
     return {
+      // 是否是编辑模式
+      isEdit: false
     }
   }
 }
