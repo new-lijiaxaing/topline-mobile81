aba9abecb2239966eceefced8e9c700b6f3fad56
牛亮 <nll@itcast.cn>
2019-09-08 15:28:44 +0800

    文章详情-展示文章详情

--- src/api/article.js
+++ src/api/article.js
@@ -42,3 +42,8 @@ export const reportArticle = ({
     type
   })
 }
+
+// 获取文章详情，id文章的id
+export const getArticle = (id) => {
+  return request.get(`/app/v1_0/articles/${id}`)
+}
--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -3,19 +3,16 @@
     <!-- 头部导航 -->
     <van-nav-bar
       title="文章详情"
-      left-text="返回"
       fixed
+      left-text="返回"
       @click-left="$router.back()"
     />
-    <div class="article">
+    <div class="article" v-if="article">
       <!-- 文章标题 -->
-      <h2 class="article-title">这是文章的标题</h2>
+      <h2 class="article-title">{{ article.title }}</h2>
       <!-- 作者信息 -->
       <!-- 文章内容 -->
-      <div class="article-content">
-        <p>hello world</p>
-        <p>hello world</p>
-        <p>hello world</p>
+      <div class="article-content" v-html="article.content">
       </div>
       <!-- 点赞和取消 -->
     </div>
@@ -23,22 +20,43 @@
 </template>
 
 <script>
+import { getArticle } from '@/api/article'
 export default {
   name: 'Detail',
-  props: ['id']
+  props: ['id'],
+  data () {
+    return {
+      article: null
+    }
+  },
+  created () {
+    // 根据id，获取文章详情
+    this.loadData()
+  },
+  methods: {
+    // 加载文章详情
+    async loadData () {
+      try {
+        const data = await getArticle(this.id)
+        this.article = data
+      } catch (err) {
+        this.$toast.fail('获取文章详情失败')
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
 .article {
-  margin-top: 92px;
+  margin-top: 48px;
   padding: 0px 20px;
   .article-title {
-    font-size: 40px;
+    font-size: 20px;
     font-weight: bold;
   }
   .article-content {
-    font-size: 26px;
+    font-size: 14px;
   }
 }
 </style>
