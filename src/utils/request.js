c1d7c21cd5f3e48404d22f3af70b1fc94d9e5dd4
牛亮 <nll@itcast.cn>
2019-09-09 10:33:30 +0800

    refresh_token实现

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import JSONbig from 'json-bigint'
 import store from '@/store'
+import router from '@/router'
 
 // 创建一个axios的实例，设置不同的baseURL
 const instance = axios.create({
@@ -44,7 +45,37 @@ instance.interceptors.response.use(function (response) {
   // 接口返回的数据中都有data，在此处统一返回接口返回的data
   // 如果接口返回的数据中没有data，此时返回axios响应对象的data属性
   return response.data.data || response.data
-}, function (error) {
+}, async function (error) {
+  // 判断状态码是否是401
+  // console.dir(error)
+  if (error.response.status === 401) {
+    // 如果是401，使用refresh_token交换新的token
+    const refreshToken = store.state.user.refresh_token
+
+    try {
+      const response = await axios({
+        method: 'put',
+        url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${refreshToken}`
+        }
+      })
+      // 新的2小时可用的token
+      const token = response.data.data.token
+      // 存储新的token
+      store.commit('setUser', {
+        token: token,
+        refresh_token: refreshToken
+      })
+      // 重新发送上一次401的请求
+      return instance(error.config)
+    } catch (err) {
+      console.log(err)
+      // 跳转到首页
+      // 如果refresh_token过期，跳转到登录页面
+      router.push('/login')
+    }
+  }
   // Do something with response error
   return Promise.reject(error)
 })
