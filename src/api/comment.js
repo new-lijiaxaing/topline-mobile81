6f565aabd0ab75b132908f7af5fbca02367e3df7
牛亮 <nll@itcast.cn>
2019-09-09 14:42:30 +0800

    封装发布评论的api

--- src/api/comment.js
+++ src/api/comment.js
@@ -1,5 +1,6 @@
 import request from '@/utils/request'
 
+// 获取评论列表
 export const getComments = ({
   isArticle,
   // 源id，文章id或评论id
@@ -17,5 +18,21 @@ export const getComments = ({
       offset,
       limit
     }
+  })
+}
+
+// 发布评论
+export const sendComment = ({
+  // 评论的目标id（评论文章即为文章id，对评论进行回复则为评论id）	
+  target,
+  // 评论的内容
+  content,
+  // 文章id，对评论内容发表回复时，需要传递此参数，表明所属文章id。对文章进行评论，不要传此参数。
+  art_id
+}) => {
+  return request.post('/app/v1_0/comments', {
+    target,
+    content,
+    art_id
   })
 }
